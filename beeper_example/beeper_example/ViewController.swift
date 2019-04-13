2e683641bef3b183d1e3f8ecfc7461f8d919690f
Alexander Murphy <amurphy345@gmail.com>
2019-04-13 13:56:56 -0600

    adding example project

---
+++ beeper_example/beeper_example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beeper_example
+//
+//  Created by Alexander Murphy on 4/13/19.
+//  Copyright © 2019 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ beeper_example/beeper_exampleTests/beeper_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  beeper_exampleTests.swift
+//  beeper_exampleTests
+//
+//  Created by Alexander Murphy on 4/13/19.
+//  Copyright © 2019 Alexander Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import beeper_example
+
+class beeper_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
